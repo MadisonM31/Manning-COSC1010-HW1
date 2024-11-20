405d65316343c82dc84b8f3c092969dda5a1f276
Madison Manning <mmannin3@uwyo.edu>
2024-11-20 05:16:01 +0000

    p

--- Manning_Madison_hw05.py
+++ Manning_Madison_hw05.py
@@ -14,42 +14,51 @@ for column in range(1,53):
 art_book.save("dimensions.xlsx")
 
 #Setting cell colors
-black = '000000'
+black = '00000000'
 fill_blk = PatternFill(patternType = 'solid', fgColor= black)
+black_list=[]
 
-yellow = 'ffc000'
+yellow = 'ffc00000'
 fill_yel = PatternFill(patternType = 'solid', fgColor=yellow)
+yellow_list=[]
 
-pale_yellow = 'ffe669'
+pale_yellow = 'ffe66900'
 fill_pyel = PatternFill(patternType = 'solid', fgColor=pale_yellow)
+pale_yellow_list = []
 
-red = 'c00000'
+red = 'c0000000'
 fill_red = PatternFill(patternType = 'solid', fgColor=red)
+red_list = []
 
-blue = '8ea9db'
+blue = '8ea9db00'
 fill_bl = PatternFill(patternType = 'solid', fgColor=blue)
+blue_list = []
 
 #scraper
 wb = openpyxl.load_workbook("COSC1010_pixel_art.xlsx")
 sheet = wb.active
-cells = tuple(sheet['A1:BA55'])
+cells = sheet['A1:BA55']
 color_dic = {'blk': [], 'yel' : [], 'p_yel' : [], 'r' : [], 'bl' : []}
 
 for row in sheet['A1:BA55']:
     for cell in row:
-        column_letter = get_column_letter(column)
-        if cell.fill.patternType:
+        if cell.fill and cell.fill.fgColor:
             color = cell.fill.fgColor.rgb
             if color == black:
                 color_dic['blk'].append(cell.coordinate)
-            if color == yellow:
+                print(f"Cell {cell.coordinate} has color {color}")
+            elif color == yellow:
                 color_dic['yel'].append(cell.coordinate)
-            if color == pale_yellow:
+            elif color == pale_yellow:
                 color_dic['p_yel'].append(cell.coordinate)
-            if color == red:
+            elif color == red:
                 color_dic['r'].append(cell.coordinate)
-            if color == blue:
+            elif color == blue:
                 color_dic['bl'].append(cell.coordinate)
+            else:
+                print(f"blank")
+        else:
+            print(f"nothere")
 
 print(f"Black:", color_dic['blk'])
         
