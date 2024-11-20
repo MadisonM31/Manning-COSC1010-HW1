1c1d54b9207ad6a93321d84cf2bcaa3ff73adbf5
Madison Manning <mmannin3@uwyo.edu>
2024-11-20 04:46:49 +0000

    progress

--- Manning_Madison_hw05.py
+++ Manning_Madison_hw05.py
@@ -1,50 +1,56 @@
 import openpyxl
 from openpyxl.styles import Color, PatternFill
+from openpyxl.utils import get_column_letter
 
 art_book = openpyxl.Workbook()
 sheet = art_book.active
 
 #Setting cell sizes
-sheet.row_dimensions[1:55].height = 8.5
-sheet.column_dimensions['A':'BA'].width = 1
+for row in range(1, 55):
+    sheet.row_dimensions[row].height = 8.5
+for column in range(1,53):
+    column_letter = get_column_letter(column)
+    sheet.column_dimensions[column_letter].width = 1
 art_book.save("dimensions.xlsx")
 
 #Setting cell colors
-black = Color(rgb= '000000')
-fill_blk = PatternFill(patternType = 'solid', fgColor=black)
+black = '000000'
+fill_blk = PatternFill(patternType = 'solid', fgColor= black)
 
-yellow = Color(rgb = 'ffc000')
+yellow = 'ffc000'
 fill_yel = PatternFill(patternType = 'solid', fgColor=yellow)
 
-pale_yellow = Color(rgb= 'ffe669')
+pale_yellow = 'ffe669'
 fill_pyel = PatternFill(patternType = 'solid', fgColor=pale_yellow)
 
-red = Color(rgb = 'c00000')
+red = 'c00000'
 fill_red = PatternFill(patternType = 'solid', fgColor=red)
 
-blue = Color(rgb= '8ea9db')
+blue = '8ea9db'
 fill_bl = PatternFill(patternType = 'solid', fgColor=blue)
 
 #scraper
-wb = openpyxl.load_workbook("COSC1010_pixel_at.xlsx")
+wb = openpyxl.load_workbook("COSC1010_pixel_art.xlsx")
 sheet = wb.active
-cells = tuple(sheet['A1':'BA55'])
-color_dic = {'black': [], 'yellow' : [], 'pale_yellow' : [], 'red' : [], 'blue' : []}
+cells = tuple(sheet['A1:BA55'])
+color_dic = {'blk': [], 'yel' : [], 'p_yel' : [], 'r' : [], 'bl' : []}
 
-for cell in cells:
-    if cell.fill.patternType != None:
-        if cell.fill.fgColor == black:
-            color_dic['black'].append(cell.coordinate)
-        if cell.fill.fgColor == yellow:
-            color_dic['yellow'].append(cell.coordinate)
-        if cell.fill.fgColor == pale_yellow:
-            color_dic['pale_yellow'].append(cell.coordinate)
-        if cell.fill.fgColor == red:
-            color_dic['red'].append(cell.coordinate)
-        if cell.fill.fgColor == blue:
-            color_dic['blue'].append(cell.coordinate)
+for row in sheet['A1:BA55']:
+    for cell in row:
+        column_letter = get_column_letter(column)
+        if cell.fill.patternType:
+            color = cell.fill.fgColor.rgb
+            if color == black:
+                color_dic['blk'].append(cell.coordinate)
+            if color == yellow:
+                color_dic['yel'].append(cell.coordinate)
+            if color == pale_yellow:
+                color_dic['p_yel'].append(cell.coordinate)
+            if color == red:
+                color_dic['r'].append(cell.coordinate)
+            if color == blue:
+                color_dic['bl'].append(cell.coordinate)
 
-print(f"Yellow:")
-print(color_dic[yellow])
+print(f"Black:", color_dic['blk'])
         
 
